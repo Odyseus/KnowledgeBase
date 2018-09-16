c955b0684efe82d383c2c67482d298addef82cb1
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:39:08 -0300

    Added README and updated __init__.py module.

--- AppData/KnowledgeBaseApp/__init__.py
+++ AppData/KnowledgeBaseApp/__init__.py
@@ -5,3 +5,7 @@ __status__ = "(Unstable)"
 __appname__ = "Knowledge Base"
 __appdescription__ = "A CLI utility to generate and manage a personal and self-hosted *Knowledge Base* on GNU/Linux."
 __version__ = "2018-09-15 01:19:13.334"
+
+
+if __name__ == "__main__":
+    pass
---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Knowledge Base ([documentation](https://pythoncliapplications.gitlab.io/CLIApplicationsManager/includes/KnowledgeBase/index.html))
+
+A CLI utility to generate and manage a personal and self-hosted *Knowledge Base* on GNU/Linux.
