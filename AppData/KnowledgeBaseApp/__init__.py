6ba2b9651658feb7d52fd60f9d1e275f1a90e94c
Odyseus <venusyadonis@gmail.com>
2018-12-15 07:24:52 -0300

    __init__.py
    - Homogenization of the __appdescription__ variable.

--- AppData/KnowledgeBaseApp/__init__.py
+++ AppData/KnowledgeBaseApp/__init__.py
@@ -3,7 +3,7 @@
 
 __status__ = "Unstable"
 __appname__ = "Knowledge Base"
-__appdescription__ = "A CLI utility to generate and manage a personal and self-hosted *Knowledge Base* on GNU/Linux."
+__appdescription__ = "CLI utility to generate and manage a personal and self-hosted *Knowledge Base* on GNU/Linux."
 __version__ = "2018-09-15 01:19:13.334"
 
 
