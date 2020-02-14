5a9d5cb3c7f69fd35038f35143f42fb51c8aff9a
Odyseus <venusyadonis@gmail.com>
2020-02-14 03:49:39 -0300

    Bump version

--- AppData/KnowledgeBaseApp/__init__.py
+++ AppData/KnowledgeBaseApp/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 __status__ = "Unstable"
 __appname__ = "Knowledge Base"
 __appdescription__ = "CLI utility to generate and manage a personal and self-hosted *Knowledge Base* on GNU/Linux."
-__version__ = "2018-09-15 01:19:13.334"
-
+__version__ = "2020-02-14 03:47:21.875"
 
 if __name__ == "__main__":
     pass
